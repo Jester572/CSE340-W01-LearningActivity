395df750fe2c591830feb31ab37e58824577d0dd
Jester572 <jester572@yahoo.com>
2023-07-22 02:41:24 -0600

    Added all error handlers and uploaded to render

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -6,5 +6,6 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId);
 router.get("/detail/:invId", invController.buildByInventoryId);
+// File Not Found Route - must be last route in list
 
 module.exports = router;
